ba157f526af780a446bf5a731449d594632b9bb8
Cristian-Hernandez-1 <chrishernandez1399@gmail.com>
2024-01-20 11:24:16 -0800

    restructured if statements and input variables

--- recommendation_script.py
+++ recommendation_script.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import random as rd
 
 movies_df = pd.read_csv('media_data/MoviesOnStreamingPlatforms.csv')
 tvshows_df = pd.read_csv('media_data/tv_shows.csv')
@@ -12,35 +13,56 @@ tvshows_df.rename(columns={tvshows_df.columns[9]: 'Prime_Video'}, inplace=True)
 movies_df.rename(columns={movies_df.columns[9]: 'Disney'}, inplace=True)
 tvshows_df.rename(columns={tvshows_df.columns[10]: 'Disney'}, inplace=True)
 
-mood = input("whats the vibe today movie or tv show?")
+def movie(platform):
+    movie_filtered_df = pd.DataFrame()
+    if platform.lower() == "netflix":
+        movies_filtered_df = movies_df[movies_df.Netflix == 1]
+    elif platform.lower() == "hulu":
+        movies_filtered_df = movies_df[movies_df.Hulu == 1]
+    elif platform.lower() == "prime video":
+        movies_filtered_df = movies_df[movies_df.Prime_Video == 1]
+    elif platform.lower() == "disney+":
+        movies_filtered_df = movies_df[movies_df.Disney == 1]
+    else:
+        print("That platform doesn't exist to me! No offense " + platform)
+        quit() 
 
-if mood.lower() == "movie":
-    def movie(platform):
-        movie_filtered_df = pd.DataFrame()
-        if platform.lower() == "netflix":
-            movies_filtered_df = movies_df[movies_df.Netflix == 1]
-        elif platform.lower() == "hulu":
-            movies_filtered_df = movies_df[movies_df.Hulu == 1]
-        elif platform.lower() == "prime video":
-            movies_filtered_df = movies_df[movies_df.Prime_Video == 1]
-        elif platform.lower() == "disney+":
-            movies_filtered_df = movies_df[movies_df.Disney == 1]
-        else:
-            print("That platform doesn't exist to me! No offense " + platform)
-            quit() 
+    row_count = len(movie_filtered_df)
+    random_number = rd.randint(1, row_count)
+    choice = movies_filtered_df.iloc[random_number]['Title']
+    return choice
+
+def tv_show(platform):
+    tvshows_filtered_df = pd.DataFrame()
+    if platform.lower() == "netflix":
+        tvshows_filtered_df = tvshows_df[tvshows_df.Netflix == 1]
+    elif platform.lower() == "hulu":
+        tvshows_filtered_df = tvshows_df[tvshows_df.Hulu == 1]
+    elif platform.lower() == "prime video":
+        tvshows_filtered_df = tvshows_df[tvshows_df.Prime_Video == 1]
+    elif platform.lower() == "disney+":
+        tvshows_filtered_df = tvshows_df[tvshows_df.Disney == 1]
+    else:
+        print("That platform doesn't exist to me! No offense " + platform)
+        quit() 
+
+    row_count = len(tvshows_filtered_df)
+    random_number = rd.randint(1, row_count)
+    choice = tvshows_filtered_df.iloc[random_number]['Title']
+    return choice
+
+mood = input("whats the vibe today movie or tv show?")
+platform = input("what streaming platform (Netflix, Hulu, Prime Video, Disney+)")
+
+if str(mood.lower()) == "movie":
+    print(movie(platform))
+
+elif str(mood.lower()) == "tv show":
+    print(tv_show(platform))
+
 else:
-    def tv_show(platform):
-        tvshows_filtered_df = pd.DataFrame()
-        if platform.lower() == "netflix":
-            tvshows_filtered_df = tvshows_df[tvshows_df.Netflix == 1]
-        elif platform.lower() == "hulu":
-            tvshows_filtered_df = tvshows_df[tvshows_df.Hulu == 1]
-        elif platform.lower() == "prime video":
-            tvshows_filtered_df = tvshows_df[tvshows_df.Prime_Video == 1]
-        elif platform.lower() == "disney+":
-            tvshows_filtered_df = tvshows_df[tvshows_df.Disney == 1]
-        else:
-            print("That platform doesn't exist to me! No offense " + platform)
-            quit() 
-    
+    print("No clue what " + mood + " is? Quit messing with me!")
+    quit()
+
+
  
